738bdecdfb15d89f75c918acc3631089041d05ac
Jim Rogers <jimrogerz@gmail.com>
2023-07-09 13:15:52 -0700

    Fix header

--- status_builder.cc
+++ status_builder.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "mediapipe/framework/deps/status_builder.h"
+#include "status_builder.h"
 
 #include <memory>
 #include <sstream>
